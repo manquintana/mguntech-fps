8eacc557b7ccbb88e142cb285bf9525a3dcf71b5
manquintana <manuelalejandroquintana@gmail.com>
2019-11-02 13:53:44 -0300

    v 0.1 beta release

--- App.js
+++ App.js
@@ -69,8 +69,8 @@ async function loadResourcesAsync() {
   const sounds = Player.load(soundLibrary);
   await Promise.all([
     Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
+      require('./assets/images/logoHome2.png'),
+      //require('./assets/images/robot-prod.png'),
     ]),
     Font.loadAsync({
       // This is the font that we are using for our tab bar
